bf718bf59ab0bf561a9a94cd6a133f42efb87ec0
Érick Almeida <erickx22x@gmail.com>
2020-10-13 19:32:14 -0300

    Criação projeto spring para sistema de vendas farmacêutico

---
+++ src/main/java/br/edu/iff/projetoFarmaceutico/ProjetoFarmaceuticoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoFarmaceutico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFarmaceuticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFarmaceuticoApplication.class, args);
+	}
+
+}
