e20c05914f08d79d813855c73c67fcb4c0f87784
Érick Almeida <erickx22x@gmail.com>
2020-10-21 17:36:25 -0300

    adicionados comentários jpa (atributos e relacionamentos) e arrumados os problemas de column (nullable = false)

--- src/main/java/br/edu/iff/projetoFarmaceutico/model/Cliente.java
+++ src/main/java/br/edu/iff/projetoFarmaceutico/model/Cliente.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+
 @Entity
 public class Cliente implements Serializable {
     private static final long serialVersionUID = 1L;
