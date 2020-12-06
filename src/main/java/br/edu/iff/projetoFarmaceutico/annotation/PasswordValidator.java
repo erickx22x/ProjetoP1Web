be9c01851c72353ced17a67a0c1e478693ff61aa
Érick Almeida <erickx22x@gmail.com>
2020-12-05 23:46:56 -0300

    Alterado o regex do passwordvalidator

--- src/main/java/br/edu/iff/projetoFarmaceutico/annotation/PasswordValidator.java
+++ src/main/java/br/edu/iff/projetoFarmaceutico/annotation/PasswordValidator.java
@@ -9,7 +9,7 @@ public class PasswordValidator implements ConstraintValidator<PasswordValidation
     public boolean isValid(String value, ConstraintValidatorContext context) {
        if(value==null) return false;
        if(value.contains(" ")) return false;
-       return value.matches("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$");
+       return value.matches("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
        
     }  
     
